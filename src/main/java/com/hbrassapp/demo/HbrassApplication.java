4f52677365e1a7ead057e296151e5d483ef5c477
Robert <robertelikplim@gmail.com>
2021-03-09 15:27:03 -0600

    Update

---
+++ src/main/java/com/hbrassapp/demo/HbrassApplication.java
@@ -0,0 +1,13 @@
+package com.hbrassapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbrassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HbrassApplication.class, args);
+    }
+
+}
